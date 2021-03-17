4d7cecfa245a2b239953e733556f9702cc123988
Mark Raudenbush <markraud@hotmail.com>
2021-03-17 16:04:36 -0600

    started the javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@ var currentDate = moment();
 $("#currentDay").text(currentDate.format("dddd, MMMM Do"));
 
 
+var taskInfo = {
+    9am: '',
+    10am: '',
+    11am: '',
+    12pm: '',
+    1pm: '',
+    2pm: '',
+    3pm: '',
+    4pm: '',
+    5pm: '',
+}
+
+
+
