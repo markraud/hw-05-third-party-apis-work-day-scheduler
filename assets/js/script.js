b8fc6a0fd1de5d6d86724b5cde10fdc00407703f
Mark Raudenbush <markraud@hotmail.com>
2021-03-17 18:24:03 -0600

    added save icon and worked on text area

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,26 @@ var currentDate = moment();
 $("#currentDay").text(currentDate.format("dddd, MMMM Do"));
 
 
-var taskInfo = {
-    9am: '',
-    10am: '',
-    11am: '',
-    12pm: '',
-    1pm: '',
-    2pm: '',
-    3pm: '',
-    4pm: '',
-    5pm: '',
-}
+// var taskInfo = {
+//     9am: '',
+//     10am: '',
+//     11am: '',
+//     12pm: '',
+//     1pm: '',
+//     2pm: '',
+//     3pm: '',
+//     4pm: '',
+//     5pm: '',
+// }
 
 
+
+// make task area show different colors based on hour of the day
+var currentTime = moment().hour();
+console.log(currentTime); 
+
+
+
+
+// click save btn to save inserted text into local storage using taskInfo
 
